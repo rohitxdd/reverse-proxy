0c7eb64cc45590ac98134e1c4d1ef3b3e6aa3e8a
Rohit Sharma <rohitsharma998850@gmail.com>
2025-01-03 23:54:45 +0530

    adding methods to send b/w master and worker

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import cluster, { Worker } from "cluster";
 import { ConfigSchemaType } from "./configSchema";
 import http from "node:http";
+import { WorkerMessageType } from "./server-schema";
 
 interface CreateServerConfig {
   port: number;
@@ -13,22 +14,41 @@ export async function createServer(config: CreateServerConfig) {
     console.log("Master cluster is up");
     console.log(config.workerCount);
     for (let index = 0; index < config.workerCount; index++) {
-      const w = cluster.fork({
+      cluster.fork({
         config: JSON.stringify(config.config),
         workerId: index + 1,
-      });
-      w.on("message", function (data) {
-        console.log(data);
       });
       console.log(`worker ${index + 1}`);
     }
     const server = http.createServer(function (req, res) {
-      console.log(Object.values(cluster.workers!).length);
+      const len = Object.values(cluster.workers!).length;
+      const workerIndex = Math.floor(Math.random() * len);
+      const w: Worker = Object.values(cluster.workers!)[workerIndex]!;
+
+      const payload: WorkerMessageType = {
+        requestType: "HTTP",
+        headers: req.headers,
+        body: null,
+        url: req.url as string,
+      };
+
+      w.send(JSON.stringify(payload));
+
+      w.on("message", (message) => {
+        res.statusCode = 200;
+        res.write(message);
+        res.end();
+      });
     });
-    server.listen(config.config.server.listen);
+    server.listen(config.config.server.listen, () =>
+      console.log("rev proxy is listening")
+    );
   } else {
     const { config, workerId } = process.env;
     const validConfig = JSON.parse(config!) as ConfigSchemaType;
     console.log(`worker node ${workerId}`);
+    process.on("message", function (d) {
+      process.send!(`Message from ${workerId}`);
+    });
   }
 }
